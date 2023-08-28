37c79ebc60a494db5bd1318ac3caf06eb1b60675
MartinMyr <martin.myrmarker@gmail.com>
2023-08-28 20:51:06 +0200

    begun with routing

---
+++ pages/add-player/index.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "@/app/layout"
+
+export default function AddPlayer() {
+  return (
+    <RootLayout>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+        </main>
+    </RootLayout>
+    
+  )
+}
